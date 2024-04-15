a00c51a838feade50090354d44458afbacf9fa15
hassen <hassen@hassen.com>
2024-04-16 03:32:55 +0900

    blu oped updator complete

--- audio_search/_op_ed_json_generator.py
+++ audio_search/_op_ed_json_generator.py
@@ -1,38 +1,123 @@
 import json
 import re
 from collections import OrderedDict
-
+from enum import Enum
 
 blu_ep_type = "blu"
 blu_eye_dict = {}
-
 ## 블루레이 아이캐치 딕셔너리 불러오기
-
 with open(f"{blu_ep_type}_eye_time.json", "r") as f:
     blu_eye_dict = json.load(f, object_pairs_hook=OrderedDict)
 
 ## 테스트
 #print(blu_eye_dict["blu_ep1"])
 
-blu_opep_time_dict_filename = "blu_opep_time"
-blu_opep_time_dict = {}
+blu_op_dict = {
+    "blu_intro" : {
+        "start" : 1,
+        "end" : 34,
+        "time" : "00:01:17:02"
+    },
+    "I_AM" : {
+        "start" : 35,
+        "end" : 64,
+        "time" : "00:01:36:01"
+    },
+    "Endless" : {
+        "start" : 65,
+        "end" : 95,
+        "time" : "00:01:36:01"
+    },
+    "Grip" : {
+        "start" : 96,
+        "end" : 127,
+        "time" : "00:02:00:01"
+    },
+    "Special-I_AM" : {
+        "start" : 128,
+        "end" : 128,
+        "time" : "00:01:36:01"
+    },
+    "new_intro" : {
+        "start" : 129,
+        "end" : 153,
+        "time" : "00:01:36:02"
+    },
+    "Angelus" : {
+        "start" : 154,
+        "end" : 167,
+        "time" : "00:02:00:01"
+    },
+}
 
-with open(f"{blu_opep_time_dict_filename}.json", "r") as f:
-    blu_opep_time_dict = json.load(f, object_pairs_hook=OrderedDict)
+blu_ed_dict = {
+    "MyWill" : {
+        "start" : 1,
+        "end" : 20,
+        "time" : "00:21:51.640"
+    },
+    "DeepWoods" : {
+        "start" : 21,
+        "end" : 41,
+        "time" : "00:21:51.640"
+    },
+    "Dearest" : {
+        "start" : 42,
+        "end" : 60,
+        "time" : "00:22:10"
+    },
+    "TruePoetry" : {
+        "start" : 61,
+        "end" : 108,
+        "time" : "00:22:10.000"
+    },
+    "ItasuraNaKiss" : {
+        "start" : 109,
+        "end" : 127,
+        "time" : "00:22:10.000"
+    },
+    "Come" : {
+        "start" : 128,
+        "end" : 146,
+        "time" : "00:20:42.000"
+    },
+    "TheTruthUntold" : {
+        "start" : 147,
+        "end" : 166,
+        "time" : "00:24:40.000"
+    },
+    "Special" : {
+        "start" : 167,
+        "end" : 167,
+        "time" : "00:21:04.000"
+    },
+}
 
+op_end_time_dict = {
+    "blu_intro" : "00:01:17:02"
+}
+
+
+## 오프닝 엔딩 시간 업데이트
 merged_blu_dict = {}
 
-for ep in range(1,2):
-    print(blu_eye_dict[f"blu_ep{ep}"])
-    print()
-    print(blu_opep_time_dict[f"blu_ep{ep}"])
+for key, value in blu_op_dict.items():
+    start_value = value.get("start")
+    end_value = value.get("end")
+    for ep in range(start_value,end_value+1):
+        blu_eye_dict[f"blu_ep{ep}"]["op_type"] = key
+        blu_eye_dict[f"blu_ep{ep}"]["op_start"] = blu_op_dict[key]["time"]
 
-
-
+for key, value in blu_ed_dict.items():
+    start_value = value.get("start")
+    end_value = value.get("end")
+    for ep in range(start_value,end_value+1):
+        blu_eye_dict[f"blu_ep{ep}"]["ed_type"] = key
+        blu_eye_dict[f"blu_ep{ep}"]["ed_start"] = blu_ed_dict[key]["time"]
 
 ## 저장
-# with open(f"{ep_type}_eye_time.json", "w") as f:
-#     sorted_keys = sorted(episode_time_dict.keys(), key=lambda x: int(re.search(r'\d+', x).group()))
-#     sorted_data = {key: episode_time_dict[key] for key in sorted_keys}
-#     #print(sorted_keys)
-#     json.dump(sorted_data, f, indent=4)
+with open(f"{blu_ep_type}_merged_time.json", "w") as f:
+    # sorted_keys = sorted(blu_eye_dict.keys(), key=lambda x: int(re.search(r'\d+', x).group()))
+    # sorted_data = {key: blu_eye_dict[key] for key in sorted_keys}
+    # #print(sorted_keys)
+    json.dump(blu_eye_dict, f, indent=4)
